0feed338c9b81938ce3718c1821f3bcb38cdaeaf
Infamia2334 <53487995+Infamia2334@users.noreply.github.com>
2020-09-07 12:55:42 +0530

    Added Functionality to filter news feed by Votes
    Used pretty printing to clean up output data, filtered only the news feeds that have over a 100 points. Also sorted the votes in a decreasing order.
    Possibilty of adding more pages remains feasible.....

--- scrape.py
+++ scrape.py
@@ -1,11 +1,21 @@
 #Using Beautiful Soup
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 res = requests.get("https://news.ycombinator.com/news")
+#res2 = requests.get("https://news.ycombinator.com/news?p=2")
 parser = BeautifulSoup(res.text, "html.parser")
+# parser2 = BeautifulSoup(res2.text, "html.parser")
 news = parser.select(".storylink")
 subtext = parser.select(".subtext")
+
+
+
+def sort_by_points(hn_list):
+    return(sorted(hn_list, key= lambda k:k["votes"], reverse=True))
+
+
 
 def create_custom_hackerNews(news, scores):
     hn = []                                                                         #HackerNews List
@@ -15,11 +25,12 @@ def create_custom_hackerNews(news, scores):
         votes = subtext[i].select(".score")
         if len(votes):
             points = int(votes[0].getText().replace(" points", ""))
-            print(points)
-            hn.append({"title": title, "links": links, "votes": points})
+            #print(points)
+            if(points >= 100):
+                hn.append({"title": title, "links": links, "votes": points})
         
-    return hn
+    return sort_by_points(hn)
     #pass
 
 
-print(create_custom_hackerNews(news, subtext))
+pprint.pprint(create_custom_hackerNews(news, subtext))
